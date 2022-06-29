a2344060babc29871634e6bfe740fa205da9e735
Derek <99042983+derekberkley@users.noreply.github.com>
2022-06-29 17:07:31 -0500

    spring lecture work

--- src/main/java/com/codeup/groovy_spring/controllers/HelloController.java
+++ src/main/java/com/codeup/groovy_spring/controllers/HelloController.java
@@ -1,27 +1,39 @@
-//package com.codeup.groovy_spring.controllers;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.*;
-//
-//@Controller
-//class HelloController {
-//
-//    @GetMapping("/groovy")
-//    @ResponseBody
-//    public String hello() {
-//        return "Hello from Spring!";
-//    }
-//
-//    @GetMapping("/hello/{name}")
-//    @ResponseBody
-//    public String sayHello(@PathVariable String name) {
-//        return "Hello " + name + "!";
-//    }
-//
-//    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
-//    @ResponseBody
-//    public String addOne(@PathVariable int number) {
-//        return number + " plus one is " + (number + 1) + "!";
-//    }
-//
-//}
+package com.codeup.groovy_spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+class HelloController {
+
+    @GetMapping("/groovy")
+    @ResponseBody
+    public String hello() {
+        return "Hello from Spring!";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "Hello";
+    }
+
+    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addOne(@PathVariable int number) {
+        return number + " plus one is " + (number + 1) + "!";
+    }
+
+    @GetMapping("/join")
+    public String showJoinForm() {
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
+    }
+
+}
