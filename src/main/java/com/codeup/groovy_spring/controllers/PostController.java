1510e32ed4a770296c0589de3746a56c740707d6
Derek <99042983+derekberkley@users.noreply.github.com>
2022-06-29 15:39:59 -0500

    finished Spring Controllers exercise

---
+++ src/main/java/com/codeup/groovy_spring/controllers/PostController.java
@@ -0,0 +1,33 @@
+package com.codeup.groovy_spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+class PostController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String indexPage() {
+        return "posts index page";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String individualPostPage(@PathVariable int id) {
+        return "view an individual post for " + id;
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String formCreatePost() {
+        return "view the form for creating a post";
+    }
+
+    @PostMapping("/posts/create")
+    public String createPost(@RequestBody Post post) {
+        return "create a new post";
+
+    }
+
+}
